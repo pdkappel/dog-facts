f19297d8743e46ef6c671c7ff5a810871fb62702
iLama <tjsimpson+github@gmail.com>
2016-04-15 16:28:49 -0700

    Initial Version
    Pretty much all there is to this

---
+++ dog-facts.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json');
+
+# Grab some of the values from the slash command, create vars for post back to Slack
+$command = $_POST['command'];
+$text = $_POST['text'];
+$token = $_POST['token'];
+# Check the token and make sure the request is from our team 
+if($token != 'adsfasdfasdfadf'){ #replace this with the token from your slash command configuration page
+  $msg = "The token for the slash command doesn't match. Check your script.";
+  die($msg);
+  echo $msg;
+}
+
+require_once('./facts.php');
+$facts_size = count($facts_list)-1;
+$rand_seeder = mt_rand(0, $facts_size);
+$rand_keys = array_rand($facts_list, $rand_seeder);
+$fact = array_rand($rand_keys, 1);
+
+$response_array = array(
+    'response_type'=> 'in_channel',
+    'text'=> $facts_list[$fact],
+    'attachments'=> array(
+            'text'=> $facts_list[$fact]
+        )
+);
+
+echo json_encode($response_array);
---
+++ facts.php
@@ -0,0 +1,63 @@
+<?php
+$facts_list = array(
+    'Is it a duck…or a dog? The Newfoundland breed has a water resistant coat and webbed feet. This dog was originally bred to help haul nets for fishermen and rescuing people at risk of drowning.',
+    
+    'It pays to be a lap dog. Three dogs (from First Class cabins!) survived the sinking of the Titanic – two Pomeranians and one Pekingese.',
+    
+    'A Beatles hit. It’s rumored that, at the end of the Beatles song, "A Day in the Life," Paul McCartney recorded an ultrasonic whistle, audible only to dogs, just for his Shetland sheepdog.',
+    
+    'Wow, check out those choppers! Puppies have 28 teeth and normal adult dogs have 42.',
+    
+    'Chase that tail! Dogs chase their tails for a variety of reasons: curiosity, exercise, anxiety, predatory instinct or, they might have fleas! If your dog is chasing his tail excessively, talk with your vet.',
+    
+    'Seeing spots? Or not… Dalmatian puppies are pure white when they are born and develop their spots as they grow older.',
+    
+    'Dogs do dream! Dogs and humans have the same type of slow wave sleep (SWS) and rapid eye movement (REM) and during this REM stage dogs can dream. The twitching and paw movements that occur during their sleep are signs that your pet is dreaming',
+    
+    'No night vision goggles needed! Dogs’ eyes contain a special membrane, called the tapetum lucidum, which allows them to see in the dark.',
+    
+    'Pitter patter. A large breed dog’s resting heart beats between 60 and 100 times per minute, and a small dog breed’s heart beats between 100-140. Comparatively, a resting human heart beats 60-100 times per minute.',
+    
+    'If your dog’s acting funny, get out the umbrella! According to a Petside.com/Associated Press poll, 72% of dog owners believe their dog can detect when stormy weather is on the way.',
+    
+    'It’s not a fever…A dog’s normal temperature is between 101 and 102.5 degrees Fahrenheit. How much do you know about dog health?',
+    
+    'Is something wet? Unlike humans who sweat everywhere, dogs only sweat through the pads of their feet.',
+    
+    'Here’s looking at you. Dogs have three eyelids, an upper lid, a lower lid and the third lid, called a nictitating membrane or “haw,” which helps keep the eye moist and protected.',
+    
+    'Americans love dogs! 62% of U.S. households own a pet, which equates to 72.9 million homes',
+    
+    'Move over Rover! 45% of dogs sleep in their owner’s bed (we’re pretty sure a large percentage also hog the blankets!)',
+    
+    'Why are dogs’ noses so wet? Dogs’ noses secrete a thin layer of mucous that helps them absorb scent. They then lick their noses to sample the scent through their mouth.',
+    
+    'Yummy! Dogs have about 1,700 taste buds. Humans have approximately 9,000 and cats have around 473.',
+    
+    'Watch that plate of cookies! A Dog’s sense of smell is 10,000 – 100,000 times more acute as that of humans.',
+    
+    'It’s not so black and white. It’s a myth that dogs only see in black and white. In fact, it’s believed that dogs see primarily in blue, greenish-yellow, yellow and various shades of gray.',
+    
+    'Did you hear that? Sound frequency is measured in Hertz (Hz). The higher the Hertz, the higher-pitched the sound. Dogs hear best at 8,000 Hz, while humans hear best at around 2,000 Hz.',
+    
+    'Express yourself. Dogs’ ears are extremely expressive. It’s no wonder! There are more than a dozen separate muscles that control a dog’s ear movements.',
+    
+    'Growing up. While the Chow Chow dogs are well known for their distinctive blue-black tongues, they’re actually born with pink tongues. They turn blue-black at 8-10 weeks of age.',
+    
+    'Why do they do that? When dogs kick after going to the bathroom, they are using the scent glands on their paws to further mark their territory.',
+    
+    'No, it’s not just to make themselves look adorable. Dogs curl up in a ball when they sleep due to an age-old instinct to keep themselves warm and protect their abdomen and vital organs from predators.',
+    
+    'Breathe easy. In addition to sweating through their paw pads, dogs pant to cool themselves off. A panting dog can take 300-400 breaths (compared to his regular 30-40) with very little effort.',
+    
+    'Dogs\' sense of hearing can reach four times the distance of humans.',
+    
+    'The phrase "It\'s Raining Cats and Dogs" was born in Germany in the 17th century. When storms flooded the streets, dogs and cats were often pulled into the currents.',
+    
+    'To differentiate between their different types of food, dogs depend more on their sense of smell than their sense of taste. Humans have five times the amount of taste buds than dogs.',
+    
+    'The Prairie Dog is actually part of the squirrel family. Early settlers named them after canines because their calls sounded like barking dogs.',
+    
+    'At the end of the song "A Day in the Life," Paul McCartney recorded a high-pitched whistle that only dogs can hear! This was done specially for his pup, Martha'
+);
+?>
